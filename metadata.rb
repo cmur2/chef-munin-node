319b1f7b64e1908cb54aafa0d81215024f540fa1
cn <cn@mycrobase.de>
2013-06-04 21:17:57 +0200

    Bump version

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email "cn@mycrobase.de"
 license          "Apache 2.0"
 description      "Installs munin-node and allows much flexibility and control while configuring it."
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          "1.0.0"
+version          "1.1.0"
 
 depends "git"
 
